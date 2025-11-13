4627af9e2d2ebc4c397242d67ba76403b1f7e4e8
kaushikthe1 <49394737+kaushikthe1@users.noreply.github.com>
2025-11-13 19:05:15 +0530

    Fix typo in dataset reference in app description

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ st.markdown("<div class='title'>COVID-19 Survival Prediction</div>", unsafe_allo
 st.markdown(
     """
     <div class='description'>
-        A combination of inflammatory and hematological markers strongly predicts mortality in COVID-19 patients, as detailed by Parul Chopra et al.(PMID: 37124653) in their study. Using the same datset, we have developed a SVM model, with accuracy of predicting survival: 80.1%, Sensitivity of 83.7%, and a specificity of 78.6%. These findings highlight the significance of laboratory markers in early prognostication and patient management.
+        A combination of inflammatory and hematological markers strongly predicts mortality in COVID-19 patients, as detailed by Parul Chopra et al.(PMID: 37124653) in their study. Using the same dataset, we have developed a SVM model, with accuracy of predicting survival: 80.1%, Sensitivity of 83.7%, and a specificity of 78.6%. These findings highlight the significance of laboratory markers in early prognostication and patient management.
     </div>
     """,
     unsafe_allow_html=True
